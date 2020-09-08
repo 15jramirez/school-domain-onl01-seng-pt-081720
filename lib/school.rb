c102caa2f61ee5367d46b375b9dd1a16c5f6a7bd
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-08 02:00:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :roster, :name, :grade
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade_level)
+    if @roster.include? (grade_level)
+    @roster[grade_level] << name
+  else
+    @roster [grade_level] = [name]
+    end
+  end
+  def grade (grade_level)
+    @roster[grade_level]
+end
+
+end
